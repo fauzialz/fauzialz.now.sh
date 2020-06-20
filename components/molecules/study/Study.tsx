1988b44c09006257769e09f98312376102bdb2ed
Fauzi Al Aziz <alaziz.fauzi@gmail.com>
2020-06-21 01:03:05 +0700

    New style for education section card list.

--- components/molecules/study/Study.tsx
+++ components/molecules/study/Study.tsx
@@ -1,34 +1,33 @@
 import styles from './Study.module.scss'
-import Link from 'next/link'
 import { study } from '../../../content/profile'
 import StudyCard from '../../atoms/studyCard'
-import BioSection from '../../templates/bioSection'
 import { subMenu } from '../../../content/router'
 import Center from '../../atoms/center'
 import { eduDetail } from '../../../pages/bio/studies'
+import InternalLink from '../../atoms/internalLink'
 
 const eduTail = 'In this section my two latest education stages are shown.'
 
 const Study = () => (
-    <Center>
-        <section id="study">
-            <BioSection
-                title="Formal Education"
-                detail={eduDetail.charAt(0).toUpperCase() + eduDetail.slice(1) + ' ' + eduTail}
-                seeDetail="See full study record"
-                seeDetailHref={subMenu.edu.href}
-            >
-                <div className={styles.timeline}>
+    <section id="study" className={styles.base}>
+        <Center noPaddingPhone>
+            <div className={styles.center}>
+                <h1 className={styles.title}>Formal Education</h1>
+                <div className={styles.card}>
+                    <p className={styles.detail}>{eduDetail.charAt(0).toUpperCase() + eduDetail.slice(1) + ' ' + eduTail}</p>
+                    <InternalLink href={subMenu.edu.href}>See full study record</InternalLink>
+                </div>
+                <div className={styles.socket}>
                     <div className={styles.line} />
-                    <div className={styles.study_list}>
-                        <div className={styles.study_blank} />
+                    <div className={styles.dummy} />
+                    <div className={styles.cards}>
                         <StudyCard school={study[study.length - 2]} />
                         <StudyCard school={study[study.length - 1]} />
                     </div>
                 </div>
-            </BioSection>
-        </section>
-    </Center>
+            </div>
+        </Center>
+    </section>
 )
 
 export default Study
