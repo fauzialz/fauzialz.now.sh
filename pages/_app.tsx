dc7f40b544ae67f967a4255c1ec89914db788d2d
Fauzi Al Aziz <alaziz.fauzi@gmail.com>
2020-07-12 20:36:57 +0700

    Firebase analytics init.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,18 @@
 import '../style/global.scss'
+import * as firebase from 'firebase/app'
+import 'firebase/analytics';
+import { firebaseConfig } from '../content/firebase'
+import { useEffect } from 'react'
 
 export default function MyApp({ Component, pageProps }) {
+
+    useEffect(() => {
+        if (!firebase.apps.length) {
+            firebase.initializeApp(firebaseConfig);
+            console.log(firebase.apps)
+        }
+        firebase.analytics()
+    }, [])
+
     return <Component {...pageProps} />
 }
